5faf440d48196967f0ba2c213ad0afe3c0a11c10
Magyari Zsolt <magyarizsolt@192-168-0-186.rdsnet.ro>
2022-10-17 17:04:01 +0300

    Missing tailwind classes config

--- src/components/layout/Categories.tsx
+++ src/components/layout/Categories.tsx
@@ -2,22 +2,30 @@ import Data from '../../assets/categoriesData.json';
 import CategoryCard from '../core/CategoryCard';
 
 const Categories: React.FC = () => {
-    return (
-        <div className=" m-auto w-full mt-32 mb-10 overflow-x-hidden">
-            <h2 className=" m-auto w-2/3 font-bold text-3xl text-black text-shadow-title"><span className="text-blue-700">Explore</span> By Category</h2>
-            <div className=" flex flex-row w-max flex-wrap gap-5 mt-12">
-                {
-                    Data.map( data =>{
-                        return(
-                            <div className={``} key={data.id}>
-                                <CategoryCard id={data.id} icon={data.icon} title={data.title} description={data.description} colorActive={data.colorActive} colorBasic={data.colorBasic} />
-                            </div>
-                        )
-                    })
-                }
+  return (
+    <div className=" m-auto w-full mt-32 mb-10 overflow-x-hidden">
+      <h2 className=" m-auto w-2/3 font-bold text-3xl text-black text-shadow-title">
+        <span className="text-blue-700">Explore</span> By Category
+      </h2>
+      <div className=" flex flex-row w-max flex-wrap gap-5 mt-12">
+        {Data.map((item) => {
+          console.log(item);
+          return (
+            <div className={``} key={item.id}>
+              <CategoryCard
+                id={item.id}
+                icon={item.icon}
+                title={item.title}
+                description={item.description}
+                colorActive={item.colorActive}
+                colorBasic={item.colorBasic}
+              />
             </div>
-        </div>
-    )   
-}
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default Categories;
